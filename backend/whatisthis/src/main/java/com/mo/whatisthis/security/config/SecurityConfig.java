fa02b31d428506c4c5541553d58f344697808f1a
김규리 <mandarining0918@gmail.com>
2023-09-11 16:09:18 +0900

    ✨feat: Setting Init Security Config(about AUTH_BLACK_LIST)

--- backend/whatisthis/src/main/java/com/mo/whatisthis/security/config/SecurityConfig.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/security/config/SecurityConfig.java
@@ -9,30 +9,58 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsUtils;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // Security 활성화, Spring FilterChain에 등록됨
 public class SecurityConfig {
 
     private final String[] AUTH_WHITELIST = {"/swagger-ui.html", "/webjars/**",
         "/file/**", "/swagger-resources/**", "/swagger/**", "/swagger-ui/**"};
 
-    @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest)
-                .permitAll()
+    private final String [] AUTH_BLACK_LIST = {"/api/v1/private/**"};
 
-                .and()
-                .cors()
-                .configurationSource(request -> {
-                    CorsConfiguration cors = new CorsConfiguration();
-                    cors.setAllowedOrigins(Arrays.asList("*"));
-                    cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    cors.setAllowedHeaders(Arrays.asList("*"));
-                    return cors;
-                });
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .requestMatchers(CorsUtils::isPreFlightRequest)
+            .permitAll()
+
+            .and()
+            .cors()
+            .configurationSource(request -> {
+                CorsConfiguration cors = new CorsConfiguration();
+                cors.setAllowedOrigins(Arrays.asList("*"));
+                cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                cors.setAllowedHeaders(Arrays.asList("*"));
+                return cors;
+            })
+
+            .and()
+            .authorizeRequests()
+            .antMatchers(AUTH_BLACK_LIST)
+            .authenticated()
+            .anyRequest()
+            .permitAll()
+
+            .and()
+            .csrf()
+            .disable()
+            .httpBasic()
+            .disable()
+            .formLogin()
+            .disable();
+
+//            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+//                UsernamePasswordAuthenticationFilter.class)
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+//            .and()
+//            .exceptionHandling()
+//            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//            .accessDeniedHandler(jwtAccessDeniedHandler)
+
+
 
         return http.build();
     }
