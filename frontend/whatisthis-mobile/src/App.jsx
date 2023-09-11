e1be9d340baa9ccc2be1206f28fe1652fe11ca90
조병철 <zxas1245@naver.com>
2023-09-11 11:39:51 +0900

    ✨feat: Create Home/Login page logic
    Home/Login 페이지 기본 UI 제작

--- frontend/whatisthis-mobile/src/App.jsx
+++ frontend/whatisthis-mobile/src/App.jsx
@@ -1,8 +1,35 @@
+import React, { useEffect, useReducer, useRef } from "react";
+
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Connection from "./pages/Connection";
+import ConnectionResult from "./pages/ConnectionResult";
+import HouseDetail from "./pages/HouseDetail";
+import HouseList from "./pages/HouseList";
+import HouseResult from "./pages/HouseResult";
+import Search from "./pages/Search";
+import SearchDetail from "./pages/SearchDetail";
+
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/connection" element={<Connection />} />
+          <Route path="/connectionresult" element={<ConnectionResult />} />
+          <Route path="/house/:id" element={<HouseDetail />} />
+          <Route path="/houselist" element={<HouseList />} />
+          <Route path="/house/:id/result" element={<HouseResult />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/search/:id" element={<SearchDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/whatisthis-mobile/src/pages/HouseResult.jsx
@@ -0,0 +1,9 @@
+const HouseResult = () => {
+  return (
+    <div className="HouseResult">
+      <h2>Here is HouseResult</h2>
+    </div>
+  );
+};
+
+export default HouseResult;
