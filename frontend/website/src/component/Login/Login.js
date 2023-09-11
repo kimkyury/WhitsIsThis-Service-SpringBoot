4f4e357a449be65c3689d24044e38f67c1b98fcd
홍진환 <ghdtka1218@gmail.com>
2023-09-11 17:56:29 +0900

    💄feat: Add Login component

--- frontend/website/src/component/Login/Login.js
+++ frontend/website/src/component/Login/Login.js
@@ -8,18 +8,18 @@ function Login(props){
         <p style={{fontWeight:'bold',color:'#2D4059', textAlign: 'center', fontSize:'40px', marginTop:'5%', marginBottom:'2%' }}>로그인</p>
         <div>
         <div className='idbox'>
-      <div style={{marginTop:'7%'}}>
-      <div style={{fontSize:'25px',width:'600px', height:'51px',display:'flex', justifyContent:'space-between'}}>
-        <label style={{fontWeight:'bold'}} for='Id'>아이디 :</label>
-        <input style={{boxShadow:'0px 3px 3px rgba(0, 0, 0, 0.5)', border:'none', backgroundColor:'#ECF0F3', width:'400px', borderRadius:'8px'}} id='Id' placeholder='아이디를 입력해주십시오.' />
-        </div>
-      <div style={{marginTop:'10px', fontSize:'25px', width:'600px', height:'51px', display:'flex', justifyContent:'space-between'}}>
-        <label style={{fontWeight:'bold'}} for='Password'>비밀번호 :</label>
-        <input style={{boxShadow:'0px 3px 3px rgba(0, 0, 0, 0.5)', border:'none', backgroundColor:'#ECF0F3', width:'400px',borderRadius:'8px'}} id='Password' placeholder='비밀번호를 입력해주십시오.' />
-    </div>
+      <div style={{marginLeft: '2.3%', marginTop:'5%'}}>
+
+
+        <input style={{boxShadow:'0px 3px 2px rgba(0, 0, 0, 0.5)', border:'none', backgroundColor:'white', width:'600px', height:'60px', borderRadius:'4px', fontSize:'25px', paddingLeft:'20px'}} id='Id' placeholder='아이디' />
+
+      {/* <div style={{marginTop:'10px', fontSize:'25px', width:'600px', height:'51px', display:'flex', justifyContent:'space-between'}}> */}
+  
+        <input style={{marginTop:'2%', boxShadow:'0px 3px 2px rgba(0, 0, 0, 0.5)', border:'none', backgroundColor:'white', width:'600px', height:'60px',borderRadius:'4px', fontSize:'25px', paddingLeft:'20px'}} id='Password' placeholder='비밀번호' />
+    {/* </div> */}
     </div>
         <div style={{ display: 'flex', justifyContent: 'center', marginBottom:'20px'}}>
-            <button style={{ fontSize:'30px',width: '600px',height:'60px',alignSelf: 'center', textAlign: 'center', borderRadius:'20px',color:'white', backgroundColor:'#2D4059'}}>로그인</button>
+            <button style={{ fontSize:'30px',width: '600px',height:'60px',alignSelf: 'center', textAlign: 'center', borderRadius:'8px',color:'white', backgroundColor:'#2D4059'}}>로그인</button>
           </div>
       </div>
         </div>
@@ -31,3 +31,4 @@ function Login(props){
 }
 
 export default Login;
+//#ECF0F3
