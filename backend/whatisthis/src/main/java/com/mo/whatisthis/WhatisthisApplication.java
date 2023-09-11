455e0ec13c4d4ccac001104995f7659a16744539
김규리 <mandarining0918@gmail.com>
2023-09-11 10:44:38 +0900

    📝docs: Add setting file content

---
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/WhatisthisApplication.java
@@ -0,0 +1,13 @@
+package com.mo.whatisthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatisthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatisthisApplication.class, args);
+	}
+
+}
