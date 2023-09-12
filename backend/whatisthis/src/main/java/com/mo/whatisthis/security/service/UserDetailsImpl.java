bf079eaf85d1ae896d08b16c65108d9c9e29d80f
김규리 <mandarining0918@gmail.com>
2023-09-12 12:28:55 +0900

    ✨feat: Implement UserDetail in Security

---
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/security/service/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.mo.whatisthis.security.service;
+
+import com.mo.whatisthis.apis.members.entities.MemberEntity;
+import java.util.ArrayList;
+import java.util.Collection;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+
+    private MemberEntity memberEntity;
+
+    public UserDetailsImpl(MemberEntity memberEntity) {
+        this.memberEntity = memberEntity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new SimpleGrantedAuthority(memberEntity.getRole()
+                                                              .getAuthority()));
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return memberEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return memberEntity.getUsername();
+    }
+
+    public MemberEntity.Role getRole() {
+        return memberEntity.getRole();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- backend/whatisthis/src/main/java/com/mo/whatisthis/security/utils/SecurityUtil.java
+++
@@ -1,5 +0,0 @@
-package com.mo.whatisthis.security.utils;
-
-public class SecurityUtil {
-
-}
