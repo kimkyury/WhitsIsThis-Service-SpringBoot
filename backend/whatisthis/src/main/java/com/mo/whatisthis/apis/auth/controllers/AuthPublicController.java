f69ca8a2e1f271ec5710a465513b69014dc942c9
김규리 <mandarining0918@gmail.com>
2023-09-12 17:20:30 +0900

    💩chore: Ready JWT Work

---
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/auth/controllers/AuthPublicController.java
@@ -0,0 +1,22 @@
+package com.mo.whatisthis.apis.auth.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthPublicController {
+
+//    @PostMapping("/employees/login")
+//    public ResponseEntity<?> employeeLogin(
+//        @RequestBody
+//
+//    ){
+//
+//    }
+
+
+}
