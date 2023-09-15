c1d5f6da56cdfdbdd458c9a2618f520731ed0db6
김규리 <mandarining0918@gmail.com>
2023-09-15 16:03:34 +0900

    💩chore: Build Logout Controller

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/auth/controllers/AuthPrivateController.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/auth/controllers/AuthPrivateController.java
@@ -1,5 +1,29 @@
 package com.mo.whatisthis.apis.auth.controllers;
 
+
+import com.mo.whatisthis.apis.auth.responses.EmployeeLoginResponse;
+import com.mo.whatisthis.apis.auth.services.AuthService;
+import com.mo.whatisthis.supports.responses.SuccessResponse;
+import com.sun.net.httpserver.Authenticator.Success;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/private/auth")
+@RequiredArgsConstructor
 public class AuthPrivateController {
 
+    private final AuthService authService;
+
+    @PostMapping("/logout")
+    public ResponseEntity<SuccessResponse> logout(
+        @RequestHeader("Authorization") String requestAccessToken) {
+        // TODO: authService logout 메소드 구현
+        return null;
+    }
 }
