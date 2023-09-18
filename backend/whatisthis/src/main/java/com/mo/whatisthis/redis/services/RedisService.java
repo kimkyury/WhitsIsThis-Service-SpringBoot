1bb03ae0205a214ec3526593a3de1f7200520c24
김규리 <mandarining0918@gmail.com>
2023-09-18 10:04:30 +0900

    ✨feat: Add getRefreshTokenKey Method

--- backend/whatisthis/src/main/java/com/mo/whatisthis/redis/services/RedisService.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/redis/services/RedisService.java
@@ -16,7 +16,7 @@ public class RedisService {
     private Long accessTokenTTL;
 
     private final RedisTemplate<String, String> redisTemplate;
-    
+
     public String getValue(String key) {
         return redisTemplate.opsForValue()
                             .get(key);
@@ -34,5 +34,10 @@ public class RedisService {
 
     public void deleteValue(String key) {
         redisTemplate.delete(key);
+    }
+
+    public String getRefreshTokenKey(String username) {
+        String returnValue = "member:" + username + ":refreshToken";
+        return returnValue;
     }
 }
