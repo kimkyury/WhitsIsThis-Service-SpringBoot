43a7f875282bc173322621e68ddb3f7c63545a1f
김규리 <mandarining0918@gmail.com>
2023-09-18 15:40:09 +0900

    🐛fix:Replace incorrectly used Annotations(Not Null)

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/auth/requests/DeviceLoginRequest.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/auth/requests/DeviceLoginRequest.java
@@ -2,9 +2,7 @@ package com.mo.whatisthis.apis.auth.requests;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 public class DeviceLoginRequest {
