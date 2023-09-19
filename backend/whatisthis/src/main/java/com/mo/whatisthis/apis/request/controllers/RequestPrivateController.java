17164df8c0c0317069ba2530a09954ed3dfae6fe
김규리 <mandarining0918@gmail.com>
2023-09-19 12:33:29 +0900

    ✨feat: Setting Swagger Tag for InspectionRequest

---
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/request/controllers/RequestPrivateController.java
@@ -0,0 +1,15 @@
+package com.mo.whatisthis.apis.request.controllers;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/private/request")
+@Tag(name = "3. InspectionRequest")
+@RequiredArgsConstructor
+public class RequestPrivateController {
+
+}
