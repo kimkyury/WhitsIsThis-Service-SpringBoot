060431dc0aadb2ca45ddd5ab958ccfd5c51219bb
조병철 <zxas1245@naver.com>
2023-09-20 15:44:19 +0900

    🔨feat: Add houseDetail loading page
    세대 상세 페이지 맵이 생성되는동안 로딩중 페이지가 나오게끔 변경

--- frontend/whatisthis-mobile/src/pages/HouseDetail.jsx
+++ frontend/whatisthis-mobile/src/pages/HouseDetail.jsx
@@ -23,6 +23,9 @@ const HouseDetail = () => {
   const [data, setData] = useState();
   const [isOpenTodoList, setIsOpenTodoList] = useState(false);
 
+  // 맵을 만드는지 확인 상태변수
+  const [isCreatingMap, setIsCreatingMap] = useState(false);
+
   useEffect(() => {
     if (buildingList.length >= 1) {
       const targetBuilding = buildingList.find((it) => parseInt(it.id) === parseInt(buildingId));
@@ -53,7 +56,8 @@ const HouseDetail = () => {
   if (!data) {
     return <div className="HouseDetail">로딩중입니다...</div>;
   } else {
-    return (
+    // 작업 상태가 도면을 만드는 중이면 도면을 만드는 로딩 사진 아니면 디테일 보여줌
+    return isCreatingMap ? (
       <div className="HouseDetail container">
         <div className="building_info_wrapper">
           <div className="building_info">
@@ -77,6 +81,42 @@ const HouseDetail = () => {
           <img src={process.env.PUBLIC_URL + `/assets/image_none.png`} alt="map" />
           <img src={process.env.PUBLIC_URL + `/assets/image_none.png`} alt="map" />
           <img src={process.env.PUBLIC_URL + `/assets/image_none.png`} alt="map" />
+        </div>
+
+        <div className="button_wrapper">
+          <MyButton text={"추가정보 입력"} color={"orange"} onClick={() => handleOpenTodoList()} />
+          <MyButton
+            text={"점검완료"}
+            color={"green"}
+            onClick={() => navigate(`/house/${buildingId}/${houseId}/result`)}
+          />
+        </div>
+
+        {/* 닫겼다가 다시 열릴때 새 페이지를 보여줄까 아니면 기존 상태 그대로 보여주는게 나을까 */}
+        {<HouseTodoList isOpen={isOpenTodoList} handleOpenTodoList={handleOpenTodoList} />}
+      </div>
+    ) : (
+      <div className="HouseDetail container">
+        <div className="building_info_wrapper">
+          <div className="building_info">
+            <div className="building_title">
+              <h1>
+                {data.dong}동{data.ho}호
+              </h1>
+              <h1 className="proecss_percentge">NN%</h1>
+            </div>
+            <h3>{addr}</h3>
+          </div>
+        </div>
+
+        <div className="loading_create_map">
+          <span className="loader" />
+          <img
+            src={process.env.PUBLIC_URL + `/assets/turtlebot.png`}
+            alt=""
+            // 임시로 바뀌게 해뒀습니다.
+            onClick={() => setIsCreatingMap(true)}
+          />
         </div>
 
         <div className="button_wrapper">
