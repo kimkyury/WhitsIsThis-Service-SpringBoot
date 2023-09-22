59dc00b91e64ccc46fa1a25d8b4cf1ee4ad2f05d
unit74 <u__nit@naver.com>
2023-09-22 15:47:57 +0900

    ⚡️ feat: Add pagenation

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/request/repositories/RequestRepository.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/request/repositories/RequestRepository.java
@@ -3,6 +3,8 @@ package com.mo.whatisthis.apis.request.repositories;
 import com.mo.whatisthis.apis.request.entities.RequestEntity;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RequestRepository extends JpaRepository<RequestEntity, Long> {
@@ -12,5 +14,5 @@ public interface RequestRepository extends JpaRepository<RequestEntity, Long> {
     List<RequestEntity> findByEmployeeIdAndStatusIn(Integer employeeId,
         List<RequestEntity.Status> statuses);
 
-    List<RequestEntity> findByStatus(RequestEntity.Status status);
+    Slice<RequestEntity> findByStatus(RequestEntity.Status status, Pageable pageable);
 }
