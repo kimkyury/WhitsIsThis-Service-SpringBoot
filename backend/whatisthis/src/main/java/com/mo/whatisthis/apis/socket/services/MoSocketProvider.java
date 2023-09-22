07693319ffae6038be03fa4fd346982940999cd3
김규리 <mandarining0918@gmail.com>
2023-09-22 16:35:12 +0900

    ✨feat: Setting DataStructure by ThreadProblem

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/socket/services/MoSocketProvider.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/socket/services/MoSocketProvider.java
@@ -1,35 +1,40 @@
-package com.mo.whatisthis.socket.services;
+package com.mo.whatisthis.apis.socket.services;
 
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.hibernate.Session;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketSession;
 
 @Component
 public class MoSocketProvider {
 
-    // 한 집엔 한 Employee만 간다는 가정
-    private static Map<Long, Session> employeeByHistoryMap = new HashMap<>();
-    private static Map<String, Session> turtleSessionByTurtleNumber = new HashMap<>();
+    private ConcurrentHashMap<Long, WebSocketSession> employeeByHistoryMap;
+    private ConcurrentHashMap<String, WebSocketSession> turtleSessionByTurtleNumber;
 
-    public void addEmployeeToSocket(Long historyId, Session employeeSession){
+    public MoSocketProvider() {
+        this.employeeByHistoryMap = new ConcurrentHashMap<>();
+        this.turtleSessionByTurtleNumber = new ConcurrentHashMap<>();
+    }
+
+    public void addEmployeeToSocket(Long historyId, WebSocketSession employeeSession) {
 
         //TODO: historyid 유효성 검사 Error처리
         employeeByHistoryMap.put(historyId, employeeSession);
+        System.out.println(">>>>>>>>>>>> Add Employee User to Map");
     }
 
-    public void removeEmployeeToSocket(Long historyId){
+    public void removeEmployeeToSocket(Long historyId) {
         employeeByHistoryMap.remove(historyId);
     }
 
-    public void addTurtleToSocket(String serialNumber, Session turtleSession){
+    public void addTurtleToSocket(String serialNumber, WebSocketSession turtleSession) {
         turtleSessionByTurtleNumber.put(serialNumber, turtleSession);
     }
 
-    public void removeTurtleToSocket(String serialNumber){
+    public void removeTurtleToSocket(String serialNumber) {
         turtleSessionByTurtleNumber.remove(serialNumber);
     }
 
