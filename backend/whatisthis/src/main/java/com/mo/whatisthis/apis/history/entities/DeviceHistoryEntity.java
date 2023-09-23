ab79e8122cbca8655d49dfe3e7402010df43d6da
unit74 <u__nit@naver.com>
2023-09-24 00:51:11 +0900

    ✨ feat: Add device history entity

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/history/entities/DeviceHistoryEntity.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/history/entities/DeviceHistoryEntity.java
@@ -12,13 +12,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "device_histories")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
 public class DeviceHistoryEntity {
 
     @Id
@@ -42,18 +40,27 @@ public class DeviceHistoryEntity {
     @Setter
     private Boolean isWorked;
 
+    public DeviceHistoryEntity(Long historyId, Float x, Float y, Category category,
+        Boolean isWorked) {
+        this.historyId = historyId;
+        this.x = x;
+        this.y = y;
+        this.category = category;
+        this.isWorked = isWorked;
+    }
+
     /*
-    AIR_CONDITIONER("에어컨", "An appliance for cooling and dehumidifying indoor air."),
-    HEATING_SYSTEM("난방 시스템", "A system designed to raise the temperature inside the building."),
-    VENTILATION_SYSTEM("환기 시스템", "A system for bringing fresh air into rooms and expelling stale or noxious air."),
-    BUILT_IN_WARDROBE("빌트인 옷장", "A wardrobe that is built directly into the walls of a room."),
-    OVEN("오븐", "An enclosed compartment for cooking and heating food."),
-    REFRIGERATOR("냉장고", "An appliance for keeping food cold."),
-    DISHWASHER("식기세척기", "A machine for washing dishes automatically."),
-    SECURITY_CAMERA("보안 카메라", "A camera that monitors and records activities in and around the building."),
-    FIRE_ALARM("화재 알람", "A device that gives a warning in case of fire."),
-    LIGHTING_SYSTEM("조명 시스템", "An arrangement of lights for illumination.");
-     */
+        AIR_CONDITIONER("에어컨", "An appliance for cooling and dehumidifying indoor air."),
+        HEATING_SYSTEM("난방 시스템", "A system designed to raise the temperature inside the building."),
+        VENTILATION_SYSTEM("환기 시스템", "A system for bringing fresh air into rooms and expelling stale or noxious air."),
+        BUILT_IN_WARDROBE("빌트인 옷장", "A wardrobe that is built directly into the walls of a room."),
+        OVEN("오븐", "An enclosed compartment for cooking and heating food."),
+        REFRIGERATOR("냉장고", "An appliance for keeping food cold."),
+        DISHWASHER("식기세척기", "A machine for washing dishes automatically."),
+        SECURITY_CAMERA("보안 카메라", "A camera that monitors and records activities in and around the building."),
+        FIRE_ALARM("화재 알람", "A device that gives a warning in case of fire."),
+        LIGHTING_SYSTEM("조명 시스템", "An arrangement of lights for illumination.");
+         */
     public enum Category {
         AIR_CONDITIONER, HEATING_SYSTEM, VENTILATION_SYSTEM, OVEN, REFRIGERATOR, DISHWASHER, SECURITY_CAMERA, FIRE_ALARM, LIGHTING_SYSTEM
     }
