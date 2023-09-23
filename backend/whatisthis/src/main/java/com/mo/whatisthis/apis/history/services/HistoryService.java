ac9b2bfb185079b1c17e47149be7d6d21a58b869
unit74 <u__nit@naver.com>
2023-09-23 16:12:42 +0900

    ✨ feat: Upload files

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/history/services/HistoryService.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/history/services/HistoryService.java
@@ -6,9 +6,11 @@ import com.mo.whatisthis.exception.CustomException;
 import com.mo.whatisthis.s3.services.S3Service;
 import com.mo.whatisthis.supports.codes.ErrorCode;
 import java.io.IOException;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +20,9 @@ public class HistoryService {
 
     private final HistoryRepository historyRepository;
 
-    public ResponseEntity<byte[]> downloadReport(Long requestId)
+    public ResponseEntity<byte[]> downloadReport(Long id)
         throws CustomException, IOException {
-        HistoryEntity historyEntity = historyRepository.findByRequestId(requestId)
+        HistoryEntity historyEntity = historyRepository.findById(id)
                                                        .orElseThrow(() -> new CustomException(
                                                            ErrorCode.BAD_REQUEST));
 
@@ -29,9 +31,9 @@ public class HistoryService {
         return s3Service.downloadFile(reportUrl, "report");
     }
 
-    public ResponseEntity<byte[]> downloadDrawing(Long requestId)
+    public ResponseEntity<byte[]> downloadDrawing(Long id)
         throws CustomException, IOException {
-        HistoryEntity historyEntity = historyRepository.findByRequestId(requestId)
+        HistoryEntity historyEntity = historyRepository.findById(id)
                                                        .orElseThrow(() -> new CustomException(
                                                            ErrorCode.BAD_REQUEST));
 
@@ -40,15 +42,54 @@ public class HistoryService {
         return s3Service.downloadFile(drawingUrl, "drawing");
     }
 
-    public ResponseEntity<byte[]> downloadZip(Long requestId)
+    public ResponseEntity<byte[]> downloadZip(Long id)
         throws CustomException, IOException {
-        HistoryEntity historyEntity = historyRepository.findByRequestId(requestId)
+        HistoryEntity historyEntity = historyRepository.findById(id)
                                                        .orElseThrow(() -> new CustomException(
                                                            ErrorCode.BAD_REQUEST));
 
         String zipUrl = historyEntity.getZipUrl();
 
         return s3Service.downloadFile(zipUrl, "zip");
+    }
+
+    @Transactional
+    public void uploadReport(Long id, MultipartFile report) throws IOException {
+        String url = s3Service.saveFile(report);
+
+        HistoryEntity historyEntity = historyRepository.findById(id)
+                                                       .orElseThrow(() -> new CustomException(
+                                                           ErrorCode.BAD_REQUEST));
+
+        historyEntity.setReportUrl(url);
+
+        historyRepository.save(historyEntity);
+    }
+
+    @Transactional
+    public void uploadZip(Long id, MultipartFile zip) throws IOException {
+        String url = s3Service.saveFile(zip);
+
+        HistoryEntity historyEntity = historyRepository.findById(id)
+                                                       .orElseThrow(() -> new CustomException(
+                                                           ErrorCode.BAD_REQUEST));
+
+        historyEntity.setZipUrl(url);
+
+        historyRepository.save(historyEntity);
+    }
+
+    @Transactional
+    public void uploadDrawing(Long id, MultipartFile drawing) throws IOException {
+        String url = s3Service.saveFile(drawing);
+
+        HistoryEntity historyEntity = historyRepository.findById(id)
+                                                       .orElseThrow(() -> new CustomException(
+                                                           ErrorCode.BAD_REQUEST));
+
+        historyEntity.setDrawingUrl(url);
+
+        historyRepository.save(historyEntity);
     }
 
 }
