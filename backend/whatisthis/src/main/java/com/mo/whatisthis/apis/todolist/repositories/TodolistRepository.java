b6a59230c2e9a30d64374979c55bdfee0a4d5e4c
unit74 <u__nit@naver.com>
2023-09-26 11:26:16 +0900

    ✨ feat: Add get history todolist

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/todolist/repositories/TodolistRepository.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/todolist/repositories/TodolistRepository.java
@@ -1,8 +1,16 @@
 package com.mo.whatisthis.apis.todolist.repositories;
 
 import com.mo.whatisthis.apis.todolist.entities.TodolistEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TodolistRepository extends JpaRepository<TodolistEntity, Long> {
+
+    @Query("SELECT t, i FROM TodolistEntity t " +
+        "LEFT JOIN FETCH TodolistOptionEntity o ON t.todolistOption.id = o.id " +
+        "LEFT JOIN FETCH TodolistImageEntity i ON t.id = i.todolistId " +
+        "WHERE t.historyId = :historyId")
+    List<Object[]> fetchTodolistWithImagesAndOptionByHistoryId(Long historyId);
 
 }
