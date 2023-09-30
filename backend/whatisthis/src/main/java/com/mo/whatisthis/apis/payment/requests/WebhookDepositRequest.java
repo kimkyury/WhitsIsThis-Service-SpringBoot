fee689759590df07d05e2e0c3ee651cdb3825220
unit74 <u__nit@naver.com>
2023-10-01 05:52:21 +0900

    🎨 refactor: Modify OffsetDateTime to String (createdAt)

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/payment/requests/WebhookDepositRequest.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/payment/requests/WebhookDepositRequest.java
@@ -1,7 +1,6 @@
 package com.mo.whatisthis.apis.payment.requests;
 
 import com.mo.whatisthis.apis.payment.entities.PaymentEntity.Status;
-import java.time.OffsetDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class WebhookDepositRequest {
 
-    private OffsetDateTime createdAt;
+    private String createdAt;
     private String secret;
     private Status status;
     private String transactionKey;
