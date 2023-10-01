1fa03069089f3fc7294e8f51db57f5699ae7f125
김규리 <mandarining0918@gmail.com>
2023-10-02 00:00:26 +0900

    ✨feat:Add State Enum Class for Socket

--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/socket/handlers/common/CommonCode.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/socket/handlers/common/CommonCode.java
@@ -2,7 +2,7 @@ package com.mo.whatisthis.apis.socket.handlers.common;
 
 public class CommonCode {
 
-    public enum SessionKey{
+    public enum SessionKey {
         HISTORY_ID, ROLE, EMPLOYEE_NO, SERIAL_NUMBER
     }
 
@@ -16,5 +16,18 @@ public class CommonCode {
 
     public enum CommandCode {
         START, END
+    }
+
+    public enum StateType {
+        WAIT_CONNECT,          // 초기에 기기가 켜진 후, 소켓 통신 연결 대기 상태
+        WAIT_WORK,                 // 소켓 통신 연결 후, 직원의 명령 대기 상태
+        WAIT_MAPPING,           // 명령을 받은 후, 터틀봇에서 지형 파악을 하기위해 움직이기 전 상태
+        MAPPING,                      // 지형 파악중
+        CALCULATE_PATH,       // 지형 파악 후, 경로 생성 중
+        WAIT_FINDING,             // 경로 생성 완료 후, 실행 전 대기 상태
+        FINDING,                        // 생성된 경로로 돌아다니는 중
+        FINISH,                            // 경로를 완주한 후 대기상태
+        ERROR,                            // 혹시 통신중 에러가 난다면 바꿔두긴 하지만 아마 기계가 꺼지도록 할 것 같습니다.
+        WORK_STOP               // 직원이 언제든 END 명령을 주었을 때, 소켓통신이 끊깁니다. }
     }
 }
--- backend/whatisthis/src/main/java/com/mo/whatisthis/apis/socket/handlers/impl/StatusMessageHandlerImpl.java
+++ backend/whatisthis/src/main/java/com/mo/whatisthis/apis/socket/handlers/impl/StatusMessageHandlerImpl.java
@@ -1,11 +1,14 @@
 package com.mo.whatisthis.apis.socket.handlers.impl;
 
 import com.mo.whatisthis.apis.socket.handlers.common.AbstractMessageHandlerInterface;
+import com.mo.whatisthis.apis.socket.handlers.common.CommonCode.CommandCode;
 import com.mo.whatisthis.apis.socket.handlers.common.CommonCode.DataType;
 import com.mo.whatisthis.apis.socket.handlers.common.CommonCode.SendType;
 import com.mo.whatisthis.apis.socket.handlers.common.CommonCode.SessionKey;
+import com.mo.whatisthis.apis.socket.handlers.common.CommonCode.StateType;
 import com.mo.whatisthis.apis.socket.services.SocketProvider;
 import com.mo.whatisthis.jwt.services.JwtTokenProvider;
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
@@ -27,6 +30,18 @@ public class StatusMessageHandlerImpl extends AbstractMessageHandlerInterface {
         String senderDevice = getAttributeAtSession(session, SessionKey.SERIAL_NUMBER);
         String receiverEmployeeNo = getAttributeAtSession(session, SessionKey.EMPLOYEE_NO);
         String historyId = getAttributeAtSession(session, SessionKey.HISTORY_ID);
+
+        String state = dataMap.get(DataType.state.name());
+
+        try {
+            StateType.valueOf(state);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            Map<String, String> errorDataMap = new HashMap<>();
+            errorDataMap.put(DataType.message.name(),
+                "State value is invalid. ");
+            String message = convertMessageToString(SendType.SYSTEM_MESSAGE, errorDataMap);
+            socketProvider.sendMessageToDevice(session, senderDevice, message);
+        }
 
         // Todo: JSON형식 유효성 검사
         saveDataAtMap(dataMap, DataType.historyId, historyId);
