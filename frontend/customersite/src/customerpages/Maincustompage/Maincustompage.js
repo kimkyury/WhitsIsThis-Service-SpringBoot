98c835539467b27261f6a73b3e1a824f2cc533ff
홍진환 <ghdtka1218@gmail.com>
2023-10-03 19:22:39 +0900

    🐛 fix: mainpage

--- frontend/customersite/src/customerpages/Maincustompage/Maincustompage.js
+++ frontend/customersite/src/customerpages/Maincustompage/Maincustompage.js
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import './Maincustompage.css';
 import { Link } from "react-router-dom";
 import { useMediaQuery } from 'react-responsive';
-// import Pdf from './Pdf';
 import Header from '../../customcomponent/customheader/customheader';
+
 function CustomMain() {
   const [logoVisible, setLogoVisible] = useState(false);
   const [buttonsVisible, setButtonsVisible] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0); // 현재 이미지 인덱스
+
+  // 이미지 파일 경로 목록
+  const imagePaths = [
+    "/assets/집2.jfif",
+    "/assets/집사진.jfif",
+    "/assets/집3.jfif",
+    // "/assets/방.svg",
+    // "/assets/방사진.svg",
+  ];
 
   useEffect(() => {
     // 로고 애니메이션 효과 시작 (1초 후)
@@ -18,7 +28,26 @@ function CustomMain() {
     setTimeout(() => {
       setButtonsVisible(true);
     }, 2500);
+
+    // 5초마다 이미지 변경
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % imagePaths.length;
+        if (nextIndex === 0) {
+          // 다음 인덱스가 0인 경우, 처음으로 돌아가기
+          return 0;
+        } else {
+          return nextIndex;
+        }
+      });
+    }, 5000);
+
+    // 컴포넌트 언마운트 시 clearInterval로 인터벌 제거
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
+
   const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minDeviceWidth: 1224 });
     return isDesktop ? children : null;
@@ -29,6 +58,7 @@ function CustomMain() {
     return isMobile ? children : null;
   };
 
+  const currentImagePath = imagePaths[currentImageIndex];
   return (
     <div>
     <Mobile>
@@ -71,34 +101,47 @@ function CustomMain() {
     <Desktop>
         <div className="roomimg fontb">
           <div className="desktop-diagonal-line">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 20 50 100" preserveAspectRatio="none">
-            <path d="M20 20 C30 70, 170 20, 100 700 V0 H100 Z" fill="#2d4059" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-13 0 50 100" preserveAspectRatio="none" width="100VW" height="100vh" >
+            <path d="M10 0 C15 180, 50 -500,50 4000 V0 H100 Z" fill="#6EC5FF" />
           </svg>
+         
           </div>
-          {/* <img
-            src={`${process.env.PUBLIC_URL}/assets/집2.jfif`}
+          <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+          <img
+            src={currentImagePath}
             alt="집 이미지"
             style={{
               height: '100%',
               position: 'absolute',
               left: '0',
               top: '0',
-              zIndex: '1',
-              width: '50vw'
+              zIndex: '999',
+              marginLeft:'15vw',
+              marginTop:'5vh',
+              width: '70vw',
+              height:'40vh',
+              border:'1.5px solid black',
             }}
           />
+          </div>
+          <div>
           <img
-            src={`${process.env.PUBLIC_URL}/assets/집3.jfif`}
+            src={currentImagePath}
             alt="집 이미지"
             style={{
               height: '100%',
               position: 'absolute',
-              width: '50vw',
-              right: '0',
+              width: '15vw',
+              height: '25vh',
+              right: '-50',
               top: '0',
-              zIndex: '1',
+              zIndex: '999',
+              marginTop: '30vh',
+              marginLeft: '16%',
+              border: '2px solid black',
             }}
-          /> */}
+          />
+          </div>
           <div className='cbox'>
             <p className={`logo-img ${logoVisible ? "show" : ""}`}>
               <img
@@ -108,7 +151,8 @@ function CustomMain() {
               />
             </p>
             <div style={{ display:'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <div style={{ zIndex:'999', display:'flex', alignItems:'center' }}>
+             
+              <div style={{marginTop:'20vh', zIndex:'999', display:'flex', alignItems:'center' }}>
                 <Link  to={'/customerreceive'}>
                   <button style={{ zIndex:'999' }} className={`cmainbutton ${buttonsVisible ? "show" : ""}`}>
                     신청하기
