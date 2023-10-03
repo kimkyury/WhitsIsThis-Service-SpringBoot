cfe9d757341ad0111895f38ce85fa95928d53d73
조병철 <zxas1245@naver.com>
2023-10-04 00:16:47 +0900

    💄docs: Change images

--- frontend/whatisthis-mobile/src/utils/WebSocket.js
+++ frontend/whatisthis-mobile/src/utils/WebSocket.js
@@ -34,7 +34,6 @@ export const WebSocketProvider = ({ children }) => {
 
     setWs(socket);
   }, []);
-  console.log(receivedMessage);
   const handleReceivedMessage = (data) => {
     setReceivedMessage(data);
   };
