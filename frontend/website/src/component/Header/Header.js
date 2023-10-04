530969ab7aa8c2f6f9be2f76e4f3b49badb24c30
unit74 <u__nit@naver.com>
2023-10-04 16:21:57 +0900

    💩 refactor: Modify CSS style

--- frontend/website/src/component/Header/Header.js
+++ frontend/website/src/component/Header/Header.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
-import './Header.css'
-import { NavLink } from 'react-router-dom';
-import axios from 'axios';
-import Logout from '../Login/Logout';
+import React, { useEffect, useState } from "react";
+import "./Header.css";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import Logout from "../Login/Logout";
 function Header(props) {
-  const [activeLink, setActiveLink] = useState('');
+  const [activeLink, setActiveLink] = useState("");
+  const [view, setView] = useState(false);
+  const [username, setUsername] = useState(null);
+  const [imageurl, setImageurl] = useState(null);
+  const [name, setName] = useState(null);
+
   const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+  useEffect(() => {
+    setUsername(sessionStorage.getItem("username"));
+    setImageurl(sessionStorage.getItem("imageurl"));
+    setName(sessionStorage.getItem("name"));
+
+    return () => {
+      setUsername(null);
+      setImageurl(null);
+      setName(null);
+    };
+  }, []);
+
   const linkStyle = {
-    color: 'black',
-    marginRight: '3%',
-    display: 'inline-block',
+    color: "black",
+    marginRight: "3%",
+    display: "inline-block",
   };
 
   const handleLinkClick = (linkName) => {
@@ -18,13 +36,16 @@ function Header(props) {
 
   // 로그아웃 함수
 
-
-
   return (
-    <div className='fontb'>
+    <div className="fontb">
       <div className="header">
-        <img style={{ marginLeft:'4rem',marginTop:'8rem', width: '8rem' }} src={`${process.env.PUBLIC_URL}/assets/logo.png`} alt='logo' /> {/* 로고 이미지를 표시합니다. */}
-        <div className='items'>
+        <img
+          style={{ marginLeft: "4rem", marginTop: "8rem", width: "8rem" }}
+          src={`${process.env.PUBLIC_URL}/assets/logo.png`}
+          alt="logo"
+        />{" "}
+        {/* 로고 이미지를 표시합니다. */}
+        <div className="items">
           {/* <NavLink
             to={`/`}
             style={activeLink === '홈' ? { ...linkStyle, color: 'orange' } : linkStyle}
@@ -32,49 +53,92 @@ function Header(props) {
           >
             홈
           </NavLink> */}
+          {/* 로그아웃 버튼 */}
+          {username && imageurl && name && (
+            <div
+              onClick={() => {
+                setView(!view);
+              }}
+              className="item"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <img
+                src={imageurl}
+                alt="User Profile"
+                style={{
+                  width: "40px",
+                  height: "50px",
+                  borderRadius: "50%",
+                }}
+              />
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <p style={{ fontSize: "12px" }}>{name}</p>
+                <Logout />
+              </div>
+            </div>
+          )}
+
           <NavLink
             to={`/list`}
-            className='item'
-            style={activeLink === '접수 목록' ? { ...linkStyle,               
-            color: '#F07B3F', 
-            height:'2rem',
-            width:'5.2rem',
-            borderBottom: '2px solid #F07B3F',
-            fontWeight: 'bold' } : linkStyle}
-            onClick={() => handleLinkClick('접수 목록')}
+            className="item"
+            style={
+              activeLink === "접수 목록"
+                ? {
+                    ...linkStyle,
+                    color: "#F07B3F",
+                    height: "2rem",
+                    width: "5.2rem",
+                    borderBottom: "2px solid #F07B3F",
+                    fontWeight: "bold",
+                  }
+                : linkStyle
+            }
+            onClick={() => handleLinkClick("접수 목록")}
           >
             접수내역
           </NavLink>
           <NavLink
             to={`/resultList`}
-            className='item'
-            style={activeLink === '결과 목록' ? { ...linkStyle,               
-            color: '#F07B3F', 
-            borderBottom: '2px solid #F07B3F',
-            height:'2rem',
-            width:'5.2rem',
-            fontWeight: 'bold'} : linkStyle}
-            onClick={() => handleLinkClick('결과 목록')}
+            className="item"
+            style={
+              activeLink === "결과 목록"
+                ? {
+                    ...linkStyle,
+                    color: "#F07B3F",
+                    borderBottom: "2px solid #F07B3F",
+                    height: "2rem",
+                    width: "5.2rem",
+                    fontWeight: "bold",
+                  }
+                : linkStyle
+            }
+            onClick={() => handleLinkClick("결과 목록")}
           >
             결과내역
           </NavLink>
           <NavLink
             to={`/mypage`}
-            className='minitem'
-            style={activeLink === '내 정보' ? { ...linkStyle,               
-            color: '#F07B3F',
-            height:'2rem',
-            width:'4rem',
-            borderBottom: '2px solid #F07B3F',
-            fontWeight: 'bold' } : linkStyle}
-            onClick={() => handleLinkClick('내 정보')}
+            className="minitem"
+            style={
+              activeLink === "내 정보"
+                ? {
+                    ...linkStyle,
+                    color: "#F07B3F",
+                    height: "2rem",
+                    width: "4rem",
+                    borderBottom: "2px solid #F07B3F",
+                    fontWeight: "bold",
+                  }
+                : linkStyle
+            }
+            onClick={() => handleLinkClick("내 정보")}
           >
             내정보
           </NavLink>
-          {/* 로그아웃 버튼 */}
-          <div style={{display:'flex', marginLeft:'12rem', marginTop:'12vh'}}>
-          <Logout/>
-          </div>
           {/* <button onClick={handleLogout}>로그아웃</button> */}
         </div>
       </div>
